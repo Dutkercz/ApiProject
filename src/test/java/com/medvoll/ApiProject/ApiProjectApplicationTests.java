0ce1a42dec5b1a2f396a574aa9b50599eaad415d
Dutkercz <dutkercz@gmail.com>
2025-02-20 18:37:22 -0300

    Initial commit

---
+++ src/test/java/com/medvoll/ApiProject/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medvoll.ApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
