98b3d075f4692e6ff55cf06ed9e74aa9b5ce401d
Dutkercz <dutkercz@gmail.com>
2025-02-20 19:11:09 -0300

    aula 1

---
+++ src/main/java/com/medvoll/ApiProject/controllers/MedicoController.java
@@ -0,0 +1,14 @@
+package com.medvoll.ApiProject.controllers;
+
+import com.medvoll.ApiProject.entities.Medico;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/medicos")
+public class MedicoController {
+
+}
