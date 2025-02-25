5402be2a060366a468fbd42f0194686eb47ad73e
Dutkercz <dutkercz@gmail.com>
2025-02-25 07:06:27 -0300

    Tratador de erros final

--- src/main/java/com/medvoll/ApiProject/entities/DTO/PacienteDTO.java
+++ src/main/java/com/medvoll/ApiProject/entities/DTO/PacienteDTO.java
@@ -1,6 +1,7 @@
 package com.medvoll.ApiProject.entities.DTO;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -9,6 +10,7 @@ public record PacienteDTO(
         @NotBlank
         String nome,
         @NotBlank
+        @Email
         String email,
         @NotBlank @Pattern(regexp = "\\d{11}")
         String cpf,
