0887d4333aff74197e052df3be96b31ee5f97e67
Dutkercz <dutkercz@gmail.com>
2025-02-28 00:14:31 -0300

    Commit final do modulo

--- src/main/java/com/medvoll/ApiProject/infra/security/SecurityFilter.java
+++ src/main/java/com/medvoll/ApiProject/infra/security/SecurityFilter.java
@@ -1,9 +1,16 @@
 package com.medvoll.ApiProject.infra.security;
 
+import com.medvoll.ApiProject.entities.usuarios.Usuario;
+import com.medvoll.ApiProject.repositories.UsuarioRepository;
+import com.medvoll.ApiProject.services.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,20 +18,34 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToekn(request);
+        if (tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            UserDetails usuario = usuarioRepository.findByLogin(subject);
 
-        System.out.println(tokenJWT);
+            var authentication = new UsernamePasswordAuthenticationToken
+                    (usuario, null, usuario.getAuthorities());
 
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
-    private Object recuperarToekn(HttpServletRequest request) {
+    private String recuperarToekn(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null){
-            throw new RuntimeException("TokenJWT não enviado no cabeçalho Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replaceAll("Bearer ", "");
         }
-        return authorizationHeader;
+        return null;
+
     }
 }
--- src/main/java/com/medvoll/ApiProject/services/TokenService.java
+++ src/main/java/com/medvoll/ApiProject/services/TokenService.java
@@ -3,6 +3,7 @@ package com.medvoll.ApiProject.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.medvoll.ApiProject.entities.usuarios.Usuario;
 import org.apache.catalina.util.Strftime;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,19 +21,32 @@ public class TokenService {
     private String secret;
 
     public String gerarToken (Usuario usuario){
-        System.out.println(secret);
         try {
             Algorithm algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
-//                    .withClaim("id", usuario.getId()) pode usar varios withClaim, no estilo chave-valor
+                    .withClaim("id", usuario.getId()) //pode usar varios withClaim, no estilo chave-valor
                     .withExpiresAt(dataExpiração())
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token");
         }
     }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
 
     private Instant dataExpiração() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
